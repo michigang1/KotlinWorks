3917a5260e086034a429b56dc752af89df71810e
Michael Chirozidi <maikl.tigriv@gmail.com>
2021-11-15 17:29:50 +0200

    Added constructor of class DateYearMonthDays and changed condition of validating input values

--- src/DateYearMonthDays.kt
+++ src/DateYearMonthDays.kt
@@ -1,16 +1,20 @@
 import java.util.Scanner
 import kotlin.math.abs
 
-class DateYearMonthDays {
+ class DateYearMonthDays(_year: Int, _month: Int, _day: Int) {
+     private var year = _year
+     private var month = _month
+     private var day = _day
 
-    fun isLeapYear (year: Int): Boolean = (year>0 && (year%4==0) )
 
-    fun monthDays (month: Int, year: Int): Int {
+    fun isLeapYear (): Boolean = (year>0 && (year%4==0) )
+
+    fun monthDays (): Int? {
         if( (month in (1..12) ) && (year > 0) ) {
             when(month){
                 1 -> return 31
                 2 -> {
-                    return if(isLeapYear(year)) 29
+                    return if(isLeapYear()) 29
                     else 28
                 }
                 3 -> return 31
@@ -25,62 +29,62 @@ class DateYearMonthDays {
                 12 -> return 31
             }
         }
-    return 0
+    return null
     }
-   fun prevDate (day: Int, month: Int, year: Int): String {
-       return if(day==1 && monthDays(month,year)==31)
+   fun prevDate (): String {
+       return if(day==1 && monthDays()==31)
                  if (month==1) ("${abs(day-31)+1}.${12-(month-1)}.${year-1}")
 
                  else ("${abs(day-31)+1}.${month-1}.$year")
 
-                 else if (day==1 && monthDays(month,year)==30) ("${abs(day-30)+1}.$month.$year")
+                 else if (day==1 && monthDays()==30) ("${abs(day-30)+1}.$month.$year")
 
                  else ("${day-1}.${month}.$year")
    }
 
-   fun nextDate (day: Int, month: Int, year: Int): String {
-       return if(day==31 && monthDays(month,year)==31)
+   fun nextDate (): String {
+       return if(day==31 && monthDays()==31)
            if (month==12) ("${abs(day-31)+1}.${month-month+1}.${year+1}")
 
            else ("${abs(day-31)+1}.${month+1}.$year")
 
-       else if (day==30 && monthDays(month,year)==30) ("${abs(day-30)+1}.${month+1}.$year")
+       else if (day==30 && monthDays()==30) ("${abs(day-30)+1}.${month+1}.$year")
 
        else ("${day+1}.${month}.$year")
    }
+
 }
 
 fun main() {
     val scan = Scanner(System.`in`)
+
     var day: Int
     var month: Int
     var year: Int
 
+
     println("Enter the year:")
     year = scan.nextInt()
-    while(year<0){
-        println("Enter  correct the year:")
-        year = scan.nextInt()
-    }
-
     println("Enter the month:")
     month = scan.nextInt()
-    while(month !in 1..12){
-        println("Enter the correct month:")
-        month = scan.nextInt()
-    }
-
     println("Enter the day:")
     day = scan.nextInt()
-    while (day !in 1..31 || ((day==29) && !DateYearMonthDays().isLeapYear(year) && month==2)) {
-        println("Enter the correct day:")
-        day = scan.nextInt()
-    }
-    
+
+    val date = DateYearMonthDays(year,month,day)
+
+    while(year<0 || month !in 1..12 || (day !in 1..31 || ((day==29) && !date.isLeapYear() && month==2))){
+
+    println("Enter  correct the year:")
+    year = scan.nextInt()
+    println("Enter the correct month:")
+    month = scan.nextInt()
+    println("Enter the correct day:")
+    day = scan.nextInt()
+}
     println("Date:$day.$month.$year")
-    println("Is leap year?-${DateYearMonthDays().isLeapYear(year)}")
-    println("Month #$month has ${DateYearMonthDays().monthDays(month,year)} days")
-    println("Prev date:${DateYearMonthDays().prevDate(day,month,year)}")
-    println("Next date:${DateYearMonthDays().nextDate(day, month, year)}")
+    println("Is leap year?-${date.isLeapYear()}")
+    println("Month #$month has ${date.monthDays()} days")
+    println("Prev date:${date.prevDate()}")
+    println("Next date:${date.nextDate()}")
 
 }
