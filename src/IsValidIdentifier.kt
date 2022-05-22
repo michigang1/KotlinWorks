f29692b22e7ce3256e570336aee6b8b8536c1ee5
Michael Chirozidi <maikl.tigriv@gmail.com>
2022-05-22 18:10:16 +0200

    Update IsValidIdentifier.kt
    some refactoring

--- src/IsValidIdentifier.kt
+++ src/IsValidIdentifier.kt
@@ -1,11 +1,12 @@
 fun isValidIdentifier(s: String): Boolean {
-   val sChar = s.toCharArray()
+   val sChars = s.toCharArray()
    var k = 0
     if(sChar.isEmpty()) k = 1
     else if(sChar[0] == '_' || sChar[0] in 'a'..'z'){
         for(ch in sChar){
            if(ch in 'a'..'z' || ch in '0'..'9' || ch == '_') k++
-           else  break }
+           else  break
+        }
    }
      return (k == sChar.size)
 }
